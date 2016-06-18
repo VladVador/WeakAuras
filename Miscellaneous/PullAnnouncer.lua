7f8a5dd0fe07ed67dd90f4328e51208d46cc58d9
Aethys256 <aethys.dev@gmail.com>
2016-06-18 20:11:08 +0200

    PullAnnouncer Update
    - Fixed Negative Pull Timer

--- Miscellaneous/PullAnnouncer.lua
+++ Miscellaneous/PullAnnouncer.lua
@@ -1,5 +1,5 @@
 --- WeakAura Strings ---
-dGuItaGik6ssfuJsaNIQ0RKkaZIs6wQu7sf)sQa1WOk6ykfzzOc9mPImnLI6AcvABsfsFdvGoNuH4DufUhQQ9jvqoOQYcfLEOumrPsxuQAJkL(iQOrkvuNuu0kfPzkQCtHYorv(jQ0qfrlLs0tf0ufYvrf0wrfWxPe4Scv9wPcK7kvaTxe9xk1GjhgLflQ6XQQMmfUSKnlcFwGgTs1PvLxtj0SPQUTuA3i9BLmCrHLRWZv00PY1ry7QKVtjOXlvOoVsH5luX(Hj3ezezylzObzObzezOXBMHpBJOQJDKHKHm6)(AKC9OoYWC9BJidF0NZ(F1MHF5kdsEBImK97El6KmIm8rFoYiYqJL9pX0TAjZsg(tmDRwYW77BKEFdXHZAaXbyJhlVFnHce)IaANDvqUfuh8O8xmdiwcxnGAxgVeVFM7TOtVi9((gPmIbZUOxNMDgLw(q8HmZXT5lNPhq33qCjCzq4GA2zuA5JuiiiO7BOKeoiZCCB(Yzc5wqgVzg(SnGC7fuBnNSGf0I6VbuI1aAJfb0Aa52lO)DgLw(qBSiGSqg1bf4I5tutO8L)Yc9Eqkeee09nKz4x8MwHm7JXnQ30kKz895YzAfYm5NLrXBAfYKtomZCDnTczYD72mB6T00kKPLFz210kKPLFz212c(SKxM8400kKzM8cJ(410B305ViZdsVVVr69nuZAuqCa24XY7xtOaD5cXJBeuexoSPZEr699nszedMDrVon71Vf3Ug5FggffIpucM)S8aPmIbZUOxNSR5m2Yi61wwHyedMDrVozxZzSLrp1i61wq8H80kKNEG077BKEFd1Sgf0wc6V7TOtOaD5cXJBeuexoSPZEr699nsVVH2sq)DVffYTGMf1aYTxqT1CYcwqmQbugJ6QOoOeed3EnG(3RFlEqAcc6V7TOqmIbZUOxNy1CYcwqbIvZjly5fPqqqqVeqmIbZUOxNMDgLw(q85dzMJBZxoti3Errkeeeeeee0lbeJyWSl61j7AoJTmIETfeIdFipHwdigXGzx0Rt21CgBz0tnIET1nNLl61wb8c1H4d5jKBVOifccccccccccckJXJ6ceRMtwW6CmHyoiZZz6gpAWtI1OL5cy2H98WXeMwHyedMDrVozxZzSLrp1i61w3CwUOxBfWR3ZXCEm96bsHGGGGGGGk)zHuiiiiiiiiiiiOmgpQlqSAozbRZX8y61dKcbbbbbbbvudKcbbbv(Z6LaIrmy2f960SZO0YhIpFiZWV4nHwdigXGzx0RtZoJslFi(8Hm7JXnQ3eAnGyedMDrVon7mkT8H4ZhYm((C5mHwdigXGzx0RtZoJslFi(8Hmt(zzu8MqRbeJyWSl61PzNrPLpeF(qMCYHzMRRj0AaXigm7IEDA2zuA5dXNpKj3TBZSP3stO1aIrmy2f960SZO0YhIpFitl)YSRj0AaXigm7IEDA2zuA5dXNpKPLFz212c(SKxM840eAnGyedMDrVon7mkT8H4ZhYmtEHrF8A6TB68xKjKBVOifccccccc6LaIrmy2f96KDnNXwgrV2ccXHpKNqRbeJyWSl61j7AoJTm6PgrV26MZYf9ARaEH6q8H8eYTxuKcbbbbbbbbbbbfwuJMDMlwnNSGvGy1CYcwNJjeZbzEot34rdEsSgTmxaZoSNhoMW0keJyWSl61j7AoJTm6PgrV26MZYf9ARaE9EoMZJPvigXGzx0RtZoJslFVEGuiiiiiiiOYFwifccccccccccckSOgn7mxSAozbRaXQ5KfSohZJPvigXGzx0RtZoJslFVEGuiiiiiiiOIAGuiiiOYFwifccccccckJXJ6cy2bzbF2jj89)O0fXFn6aHSGXIge6FNrPLp0SCqpkKBVGYK(CqH1V7TOhtVifcccQOgifcccIrmy2f960Sx)wC7AK)zyuui(qUbr5bsHGGGgLJyq9aPf1G0r6idDJxWG1GmIm0Trgfz4pX0TAjdnigm3Brjd3ez4pX0TAjdtqq)DVffkq)vrDwHsoQeVo2kuZQnpZ1FvuNviBRq2wHcxeJ)IhRTScfUig)1wFwWPviBRq2wH6wthpwBzfQBnDB9zbNwHSTczBfkmJY3NhRT8IuiiiO7BOSR5ek2YakjHVpu0RTgqDMrn8RbsHGGGEjG6VkQdIpFiZMo)fzhlKt7VUD5YZeIrnGsoQeVogIpFiZUB2eIrnGMUXJg8K4rnc0SAZZC9xf1zfYmzKPxi3ErrkeeeeeeeeJyWSl61j7AoJTmIETfeFi3Is0MVgbMUXJg8mjYhOz1MN56VkQZk0MTcfxVE9aPqqqqqqqqmIbZUOxNSR5m2YONAe9Ali(qCwUOxBfWluhaeJyWSl61j7AoJTmIETLhifcccQOgifccc6(gk5OUkQdkbXWTxdOM963ItKcbbb9sa1FvuheF(qMjZ9fFVL2w2Zzpp7EEFzZ131eYTxuKcbbbbbbbXigm7IEDA2RFlUDnY)mmkkeFOem)z5bsHGGGkQbsHGGGUVHss47d1Sx)weQZmQHFnqkeee0lbeD5GyedMDrVon71Vf3Ug5FggffIrnG6wthpwBbXOgqHlIXFXJ1wqmQbu3A64XAlieh(qHlIXFXJ1wqmQbeD5GMUXJg8K4rnc0SAZZC9xf1zfYmzVLzYTBMnMEHyudOat34rdEs8OgbAwT5zU(RI6SczA39l2hN9MEHwdOPB8ObpjEuJanR28mx)vrDwHmTJLzyyFxtVEHC7ffPqqqqqqqq33qj9zBudOpNJ9BrKcbbbbbbb9saL)5o5zuJaHlIXFT1NfCAfQHBSSFr54YPn3SP9nr2rXNS3ozUV47T0leIdFipHyudO8p3jpJAeOBnDB9zbNwHA4gl7xuoUCAZnBAFtKDu8j7T5LSXleIdFipHC7ffPqqqqqqqqqqqqVeqt34rdEs8OgbAwT5zU(RI6SczgAbzYJttVqU9IIuiiiiiiiiiiiiiiiigXGzx0RtSAozbRaHlIXFXJ1wNJjuge((LbK550TMoES26CmHSWNBh6JWTG(Co2Vfn96bsHGGGGGGGGGGGGGGGgLJyq9aPqqqqqqqqqqqqL)SqkeeeeeeeeeeeeeeeeJyWSl61jwnNSGvGWfX4V4XARZXekdcF)YaY8C6wthpwBDoMqw4ZTdzEoHzu((8yTLxpqkeeeeeeeeeeeeeee0OCedQhifcccccccccccQOgifccccccc6(gk7YydOKe((ifcccccccQ8N1lbu(N7KNrnc0TMUT(SGtRqnCJL9lkhxoT5MnTVjYok(K92jZ9fFVLEHqC4d5jeJAaL)5o5zuJaHlIXFT1NfCAfQHBSSFr54YPn3SP9nr2rXNS3MxYgVqio8H8eYTxuKcbbbbbbbbbbbXigm7IEDIvZjlyfOBnD8yT15ycLFzSbuge((LbK55eUig)fpwB51dKcbbbbbbbbbbbnkhXG6bsHGGGGGGGUVHswoOpNJ9BrKcbbbbbbbv(Z6Lak)ZDYZOgbcxeJ)ARpl40kud3yz)IYXLtBUzt7BISB4YlYsU5StM7l(El9cH4WhYtig1ak)ZDYZOgb6wt3wFwWPvOgUXY(fLJlN2CZM23ezhfFYEBEjB8cH4WhYti3ErrkeeeeeeeeeeeeJyWSl61jwnNSGvGWfX4V4XARZXmqYY5fkdcF)YaY8C6wthpwB51dKcbbbbbbbbbbbnkhXG6bsHGGGGGGGUVHswoOSlJnGss47JuiiiiiiiOYFwVeq5FUtEg1iq4Iy8xB9zbNwHA4gl7xuoUCAZnBAFtKDdxErwYnNDYCFX3BPxieh(qEcXOgq5FUtEg1iq4Iy8xB9zbNwHA4gl7xuoUCAZnBAFtKDu8j7T5LSXleIdFipHC7ffPqqqqqqqqqqqqmIbZUOxNy1CYcwb6wthpwBDoMbswoVq5xgBaLbHVFzazEoHlIXFXJ1wE9aPqqqqqqqqqqqqJYrmOEGuiiiiiiiOIAGuiiiOIAG0IAqg(tmDRwB3gzuKH1vrDKH)71VfjdRRI6idRRI6MKHnCJL9lYohxoT75a98IS5qEBZmxuVL9DTcLm3x89wABzpN98S759LnxFxRqnD(lYowiN2FD7YLhz4pX0TAT3FgfzO71wgKoYWrf8TOrBKrrg6Qo2rgMyWARq3G5yK84izOf(mC7KB3CC9StD0o1Pnhxp5yN4GDK4YrYe3XLJKHZYprY1J6idZ1VnImKs0gnEbdwJjjVnrg(midTGp7VYTVX0ojHV)hLUiO)1GmCVEb3DKB3CC9StD0o1Pnhxp5yN4GDK4YrYe3XLJKH(lMbzezOXlrI3pHVBdYiYWwcF3JmI0r6idhl)ImImSLW39iJiDKoYWeSF3BrjJidBj8DpYishPJm0X8lQJmImSLW39iJiDKoYWb7ViJidBj8DpYishPJ0r6idNKHC8SPJNhoYbjdVi5TPnZXnr6ijb
+d0ubyaGiH6sQsqJsu5uIsVssrQzrGBrIDrs)svc1WKu6ykQSmQipJkW0uu11eu2MQK(gLGXPkL6CskQ3jkCpvX(uLqoiL0cvIEOcMOiCrL0gPc9rkrJKkOtkkALc8skHmtLWnfvTtH4NcPHkPAPur9uQQPkjxvvk2QKI4RQs0zvu8wfs3vHyVuL)ksdgDysTyfQhtOMmL6YsTzr0NfKrtqNwv9AfLMnv62kYUj63Qy4cQwUepxLMofxxP2oH8DkHA8QsjNxvQwVQeO3RkbmFjfUVKIK9d7nNxLN)KNVTNVTxLNV9)gUR(9Q(TmE(E(Lo0)iREp82Z30VLXZ)l)MuXNPWDBtB7fzopFTyZ)iVEvE(F534v55BFsfVVMZK3spFX7R5m55ROOGaffW3C7cynrx(6XU9fm3mNnOqTOg0CaFXL29OTb1jnDbC6y)t(fRn)J8MfbkkkiqVl60wksDqOwkBxWhW4frhSUiodqffWOBt4Bd4GqTu2UiaeeeurbS(2agVi6G1fXGMdO9)gUR(DqJWgCQVxDOg8iDFhm5Pa((zdEkGgHnOyHAPSDbF)SbTyT0aMtK2D3xWXT7XIZQIaqqqqffWyFRZelamEnF0Q1ybGXZSUyrSaW46wDUAMybGXw(MmxKiwayCuhDmZHvNJfag7S1mtelam2zRzMi9LwDosMrSmwayCMr8RSgzynDWHwRIvrGIIccuuahoLgSMOlF9y3(cMlruWirRaRI(MbhMfbkkkiqVl60wksDqylEwhpLXFB7wc(aMu7E7mqGEx0PTuK6YZ9M)yx9NAbG6DrN2srQlp3B(J9Q0U6p1GpG1kaS2mqGEx0PTuKQF4TR7f8b8fabGGGGkkGv6T0GaqqqWrDqyHnc4dOPSBbGkkGVuxk)tbubSsVLgeaccco6RonNtZpc4dOPSBbGkkG11UA7Veub05w8Sh5xgcbGGGGJ6uTwWbVoc4dOPSBbGkkGoClnDbubmX)AkAXMVmeOVWJbbGGGGJg2Ro5KtJa(aAk7waOIcyclcubmHUml4WzAslTraiii4OZnxyHn)RJa(aAk7waOIcy(JCwqfW6LJOZSncabbbh9vluZHnc4dOPSBbGkkGjk7VnOcOLLJf7IaqqqWrN7TdZP5dBeWhqtz3cavuat4qqfWQtzQLgeacccQOa6)DBP4(fNP00LYoCdcabbbh91WcZPraFanLDlaurb8L6s5FkGkG13ttTlcabbbhTMF1jhm3iGpGMYUfaQOawx7QT)sqfqN7X7zBeaccco6CZBbh8QtJa(aAk7waOIcOd3stxavahoBPPlG(cpgeaccco6Cw41xhb8b0u2TaqffqNpH2nOcOf9fplcabbbhDElmVfEDeWhqtz3cavuatyrGkG(MIwgA7QnncabbbhDEleMtV9iGpGMYUfaQOaMWIavaZ)L2jlD)c2iaeeeC05EBhyHWCAeWhqtz3cavuat4qqfWHJ8UNAW86qFXiaeeeurbmrpENmjcabbbhD(WcZP5hb8b0u2TaqffWeweOc4Gq9xEbpjbZuCJaqqqWrNVMdZcVoc4dOPSBbGkkGjSiqfWeT2ie0YYpCeacccoQfSWR1(6iGpGMYUbvuatyrGkGj0Lzb9)tBxT4YEcb1uzGafffeOOaoCknOJBPyZ)iVG5sefms0kWQOVzWHzrGIIccuuaDClfB(hjO5aEBPnOrydo13RoudQL2GHxArT0aMCxmc7cOyHT4zvrqYTuS5FKG6DrN2srQ577vhQbZLVVxDOolcabbb)jb17IoTLIuheQLY2f85bmEr0bRlIbncBjcabbbbbbb)jb17IoTLIuxEU38h7Q)udIA8awlOwAdQ3fDAlfPU8CV5p2Rs7Q)uRyzBQ(tDUSGVOhqL5ancBjcabbbbbbbbbbbdV8LMC577vhQvvJb1gWyv1RP8LHutEktAtU4xyTQoLmwaOEx0PTuK6YZ9M)yVkTR(tTILTP6p15YMvvn(QgNndeaccccccc2U3gbGGGGGGGGGGGGHx(stU899Qd1QQXQXzZabGGGGGGGGT0gbGGGGT7T)jb17IoTLIuheQLY2f85bm236mXGNcOEx0PTuK6GqTu2UGppGXR5JwTgdEkG6DrN2srQdc1sz7c(8agpZ6IfXGNcOEx0PTuK6GqTu2UGppGX1T6C1mXGNcOEx0PTuK6GqTu2UGppGXw(MmxKig8ua17IoTLIuheQLY2f85bmoQJoM5WQZXGNcOEx0PTuK6GqTu2UGppGXoBnZeXGNcOEx0PTuK6GqTu2UGppGXoBnZePV0QZrYmILXGNcOEx0PTuK6GqTu2UGppGXzgXVYAKH10bhATkg0iSLiaeeeeeee8NeuVl60wksD55EZFSR(tniQXdyTGAPnOEx0PTuK6YZ9M)yVkTR(tTILTP6p15Yc(IEavMd0iSLiaeeeeeeeeeee0VL2dc1M899Qd15Y33RouRQgdQnGXQQxt5ldPM8uM0MCXVWAvDkzSaq9UOtBPi1LN7n)XEvAx9NAflBt1FQZLnRQA8vnwaOEx0PTuK6GqTu2UzZabGGGGGGGGT7TraiiiiiiiiiiiOFlTheQn577vhQZLVVxDOwvnwnwaOEx0PTuK6GqTu2UzZabGGGGGGGGT0gbGGGGT7Traiiiiiiiy4LV0KlE0xAnT(211QuE2I7YiGVSCKHaflulLTl4TnGFjOrydMP8Ba9BXM)rQgNfbGGGGT0gbGGGG6DrN2srQdcBXZ64Pm(BB3sWhqtz3zGaqqqWsB2fzgiOL2EgpJN)TDtw)8Lgp)fRow55RTT)M)rQDtnLFOqD56v5z881sXcpL6NV045Vy1Xkp)Kf9u7BkAJ2lItE(w832i0ZX5dRwh8QdCW8HvRtoWc1Cyo5LujmN88nLFOqDXRYZ38E4TNV491CM88fVVMZuQ59WBp)wulnE(27I28psp)588flSfpRNFlQLgp)wulnxp)HO5xATkDrultxRjRrQsFtehZCr1QZRjeawFH1zwDo15vlxJKUgX6YfRjeao4qRvP59Tm1AIerJ45lEFnNjp)KBPyZ)ibZTkQLgbG1lDY)BjaC4mnwBwf1sJaWubGPca9p7I4oIEQfa6F2fXTJU6qxbGPcatfaMOVMi6PwayI(AC0vh6kamvaOF4TRBMjea6hE76grp1zraiiiOIc4YZ9cM)ydwF76cw9N6cOd1sB3UGaqqqWFsWvrT0a(8agp4qRvP59Tm1AIerJedQL2G1lDY)Bb(8agNy(yqT0g8AkFzi1KFPDUHZ0yTzvulncaJRxfNf0iSLiaeeeeeeeuVl60wksD55EZFSR(tn4dO5i3tJ7sURP8LHuV7X5gotJ1MvrT0iaCEbGHLnBgiaeeeeeeeuVl60wksD55EZFSxL2v)Pg8b0Y2u9N6CzbRPb17IoTLIuxEU38h7Q)uNbcabbbBPncabbbvuaRxArT0aMCxmc7c4GWw8Sxeaccc(tcUkQLgWNhW46lSoZQZPoVA5AK01iwxUynrmOrylraiiiiiiiOEx0PTuK6GWw8SoEkJ)22Te8bmP292zGaqqqWwAJaqqqqffW6BxxWbHT4zbDOwA72feaccc(tckpgq9UOtBPi1bHT4zD8ug)TTBjOwAdMOVMi6PgulTb9p7I4oIEQb1sBWe91erp1GOgpG(NDrChrp1GAPnO8yaVMYxgsn5xANB4mnwBwf1sJaW46RoNz0ezoeNfulTbZDnLVmKAYV0o3WzAS2SkQLgbGXPjSM3QLRXzbpfWRP8LHut(L25gotJ1MvrT0iamonFM((RjIZcEkGxt5ldPM8lTZnCMgRnRIAPray8Gv)Qu)3mmS67hNnlOrylraiiiiiiiOIcyDx97Db0QXOfplcabbbbbbb)jbh)nQJ1s7C(NDrC7ORo0va4q08lTwTiQLPrxgSouLwnt9106lSoZQZzbrnEaRfulTbh)nQJ1s7Cj6RXrxDORaWHO5xATArultJUmyDOkTAM6RPrQpKfe14bSwqJWwIaqqqqqqqqqqqWFsWRP8LHut(L25gotJ1MvrT0iam2)LzgXY4SGgHTebGGGGGGGGGGGGGGGG6DrN2srQ577vhQZ5F2fXDe9uRQgdg(21TTbJvvt0xte9uRQgdAXFJqqRBZb0QXOfpBC2mqaiiiiiiiiiiiiiiiyPn7Imdeaccccccccccc2U3(NeuEmGxt5ldPM8lTZnCMgRnRIAPray8Gv)Qu)3mmS67hNf8ua17IoTLIu9dVDDVJ6hE76MzIrancBjcabbbbbbbbbbbbbbb17IoTLIuZ33RouNZ)SlI7i6Pwvngm8TRBBdgRQMOVMi6Pwvng0I)gHGXQQ(H3UUr0tD2mqaiiiiiiiiiiiiiiiyPn7Imdeaccccccccccc2sBeacccccccQOaU8y)oy9TRlcabbbbbbbB3B)tco(BuhRL25s0xJJU6qxbGdrZV0A1IOwMgDzW6qvA1m1xtRVW6mRoNfe14bSwqT0gC83OowlTZ5F2fXTJU6qxbGdrZV0A1IOwMgDzW6qvA1m1xtJuFiliQXdyTGgHTebGGGGGGGGGGGG6DrN2srQ577vhQZLOVMi6PwvngC8X(DWW3UUTnySQQ)zxe3r0tD2mqaiiiiiiiiiiiyPn7ImdeacccccccQOawVnGwngT4zraiiiiiiiy7E7FsWXFJ6yT0oN)zxe3o6QdDfaoen)sRvlIAzA0LbRdvPdrJu5C0fP1xyDMvNZcIA8awlOwAdo(BuhRL25s0xJJU6qxbGdrZV0A1IOwMgDzW6qvA1m1xtJuFiliQXdyTGgHTebGGGGGGGGGGGG6DrN2srQ577vhQZ5F2fXDe9uRQgNREBYcg(21TTbJvvt0xte9uNndeacccccccccccwAZUiZabGGGGGGGGkkG1Bd4YJ97G13UUiaeeeeeeeSDV9pj44VrDSwANZ)SlIBhD1HUcahIMFP1QfrTmn6YG1HQ0HOrQCo6I06lSoZQZzbrnEaRfulTbh)nQJ1s7C(NDrC7ORo0va4q08lTwTiQLPrxgSouLwnt910i1hYcIA8awlOrylraiiiiiiiiiiiOEx0PTuKA((E1H6Cj6RjIEQvvJZvVnzbhFSFhm8TRBBdgRQ6F2fXDe9uNndeacccccccccccwAZUiZabGGGGGGGGT0gbGGGGT0gbT02Zx8(AotPc)2TNV5p12EgpF5EQQ8dfQlxViZ55lS)HeA8CC(WQ1bV6ahmFy16KdSqnhMtEjvcZjpF3J22RYZ3(Nm5x8218UxLN)02189Q8mEgp)YXT9Q88N2UMVxLNXZ45B0UT04v55pTDnFVkpJNXZpPwS5FKEvE(tBxZ3RYZ4z88lAXTxLN)02189Q8mEgpJN)32Z)LwtT2gHVFtRVDDTkLNTuCx8mE(xpFNuNtTwvNSGNViViZnVtZ5z88a
 
 
 --[[
@@ -26,19 +26,41 @@ aura_env.Channel = "LOCAL"; -- Output Channel
 ---
 aura_env.CheckForbidden = false;
 aura_env.BossModTime, aura_env.BossModEndTime = 0, 0;
+aura_env.Spells = {
+	-- Taunt
+	[355] = true, -- Warrior - Taunt
+	[62124] = true, -- Paladin - Reckoning
+	[20736] = true, -- Hunter - Distracting Shot
+	[56222] = true, -- DK - Dark Command
+	[115546] = true, -- Monk - Provoke
+	[6795] = true, -- Druid - Growl
+	[185245] = true, -- DH - Torment
+	-- Silence/Interrupt
+	[6552] = true, -- Warrior - Pummel
+	[96231] = true, -- Paladin - Rebuke
+	[147362] = true, -- Hunter - Counter Shot
+	[1766] = true, -- Rogue - Kick
+	[47476] = true, -- DK - Strangulate
+	[47528] = true, -- DK - Mindfreeze
+	[183752] = true, -- DH - Consume Magic
+	-- Debuff
+	[45524] = true, -- DK - Chains of Ice
+	[49576] = true, -- DK - Death Grip
+	[77606] = true -- DK - Dark Simulacrum
+};
 ---
 -- Core Functions (DO NOT TOUCH)
 ---
 -- Function to send the message and prevent further check.
 function aura_env.Message (Message)
 	if aura_env.Channel == "LOCAL" then
-		if aura_env.BossModTime ~= 0 or aura_env.BossModEndTime-GetTime() >= 0 then
+		if aura_env.BossModTime ~= 0 and aura_env.BossModEndTime-GetTime() >= -1 then
 			print(Message.." at "..string.format("%0.2f", aura_env.BossModEndTime-GetTime()).."s.");
 		else
 			print(Message..".");
 		end
 	elseif aura_env.Channel == "SAY" or aura_env.Channel == "EMOTE" or aura_env.Channel == "YELL" or aura_env.Channel == "PARTY" or aura_env.Channel == "GUILD" or aura_env.Channel == "OFFICER" or aura_env.Channel == "RAID" or aura_env.Channel == "RAID_WARNING" or aura_env.Channel == "INSTANCE_CHAT" then
-		if aura_env.BossModTime ~= 0 or aura_env.BossModEndTime-GetTime() >= 0 then
+		if aura_env.BossModTime ~= 0 and aura_env.BossModEndTime-GetTime() >= -1 then
 			SendChatMessage(Message.." at "..string.format("%0.2f", aura_env.BossModEndTime-GetTime()).."s.", aura_env.Channel);
 		else
 			SendChatMessage(Message..".", aura_env.Channel);
@@ -55,7 +77,7 @@ end
 
 -- Trigger [COMBAT_LOG_EVENT_UNFILTERED]
 
-function (Event, Prefix, CombatEvent, _, _, SourceName, SourceFlags, _, _, DestName, DestFlags, _, _, SpellName)
+function (Event, Prefix, CombatEvent, _, _, SourceName, SourceFlags, _, _, DestName, DestFlags, _, SpellID, SpellName)
 	-- Boss Mod Pull Timer Handler
 	if Event == "CHAT_MSG_ADDON" and Prefix == "D4" and string.find(CombatEvent, "PT") then
 		aura_env.BossModTime = tonumber(string.sub(CombatEvent, 4, 5));
@@ -66,13 +88,13 @@ function (Event, Prefix, CombatEvent, _, _, SourceName, SourceFlags, _, _, DestN
 		aura_env.CheckForbidden = false;
 	end
 	-- Pull Check Handler
-	if not aura_env.CheckForbidden and DestName and SourceName and DestName ~= SourceName and not string.find(CombatEvent, "PERIODIC") and (string.find(CombatEvent, "_DAMAGE") or string.find(CombatEvent, "_MISSED")) then
+	if not aura_env.CheckForbidden and DestName and SourceName and DestName ~= SourceName and not string.find(CombatEvent, "PERIODIC") and (string.find(CombatEvent, "_DAMAGE") or string.find(CombatEvent, "_MISSED") or string.find(CombatEvent, "CAST_SUCCESS")) then
 		-- Player Attack
 		if bit.band(SourceFlags, COMBATLOG_OBJECT_TYPE_PLAYER) ~= 0 and bit.band(DestFlags, COMBATLOG_OBJECT_TYPE_NPC) ~= 0 then
 			if string.find(CombatEvent, "SWING") then
 				aura_env.Message(SourceName.." pulled "..DestName.." with Auto Attack");
 				return;
-			else
+			elseif not string.find(CombatEvent, "CAST_SUCCESS") or aura_env.Spells[SpellID] then
 				aura_env.Message(SourceName.." pulled "..DestName.." with "..SpellName);
 				return;
 			end
